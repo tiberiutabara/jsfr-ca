a159d11796a5c14726180b0d45196011d12253ba
tiberiutabara <tiberiutabara1@gmail.com>
2022-10-11 00:38:06 +0200

    head

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,8 +1,14 @@
 import Link from "next/link"
+import Head from "next/head"
 
 function Nav() {
   return (
     <ul className="nav">
+
+      <Head>
+        <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests" /> 
+      </Head>
+
         <li>
             <Link href="/">Home</Link>
         </li>
